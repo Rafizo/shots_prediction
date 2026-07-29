21d333944dcaa9730be7ecf52ceb9e715e181dc3
Rafizo <rafizoshunen@gmail.com>
2026-07-29 14:36:59 +0700

    v2

--- deployment/src/prediction.py
+++ deployment/src/prediction.py
@@ -68,7 +68,7 @@ def run():
         pred_proba = best_model.predict_proba(data_inf)
 
         #display
-        st.write("## Goal Probability :", pred_proba)
+        st.write("## Goal Probability :", int(pred_proba[0][1]))
         st.write("## Is goal? :", int(pred[0]))
 
         if int(pred[0]) == 1:
